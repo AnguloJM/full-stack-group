79d2a74caf4b2f94912eab5c0aac9cdb03a5212d
James Mernin <jamespmernin@gmail.com>
2020-10-26 12:02:10 -0400

    Changed 'e' to 'event' for naming convention

--- db/connection.js
+++ db/connection.js
@@ -5,6 +5,6 @@ let MONGODB_URI = process.env.PROD_MONGODB || 'mongodb://127.0.0.1:27017/postsAp
 mongoose
   .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false })
   .then(() => console.log('Successfully connected to MongoDB.'))
-  .catch(e => console.error('Connection error', e.message))
+  .catch(event => console.error('Connection error', event.message))
 
 module.exports = mongoose.connection
